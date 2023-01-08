b8ae740afb4d78e49f5ceae8f72e0d2a9030a12a
LeitgebMathias <mathias.leitgeb@stud.fh-campuswien.ac.at>
2023-01-08 13:36:10 +0100

    SNEK6 AND SNEK 8 - Implement slight improvements requested for the pull request.

--- src/main/java/at/ac/fhcampuswien/snake/ingameobjects/Food.java
+++ src/main/java/at/ac/fhcampuswien/snake/ingameobjects/Food.java
@@ -2,7 +2,7 @@ package at.ac.fhcampuswien.snake.ingameobjects;
 
 import at.ac.fhcampuswien.snake.util.Constants;
 
-public class Food{
+public class Food {
 
     private final Position position;
 
@@ -22,8 +22,11 @@ public class Food{
         boolean isTargetFieldFree;
         int foodXCoord;
         int foodYCoord;
-        int segmentNumberX = Constants.NUMBER_OF_ROWS_AND_COLS - 2 ;
-        int segmentNumberY = Constants.NUMBER_OF_ROWS_AND_COLS - 3 ;
+        // We reduce "2" from segmentNumberX, because there are two Colums being used for the outer walls.
+        int segmentNumberX = Constants.NUMBER_OF_ROWS_AND_COLS - 2;
+        // We reduce "3" from segmentNumbery, because there are two rows being used for the outer walls.
+        // Additionally there is 1 row used for the status bar at the top.
+        int segmentNumberY = Constants.NUMBER_OF_ROWS_AND_COLS - 3;
         do {
             isTargetFieldFree = true;
             foodXCoord = (int) (Math.random() * segmentNumberX) + 1;
@@ -35,24 +38,24 @@ public class Food{
             foodYCoord *= Constants.OBJECT_SIZE_MEDIUM;
 
             // Check if calculated Position is inhibited by the snake
-            int i=0;
+            int i = 0;
             do {
-                if(snake.getSegments().get(i).getX() == foodXCoord &&
+                if (snake.getSegments().get(i).getX() == foodXCoord &&
                         snake.getSegments().get(i).getY() == foodYCoord) isTargetFieldFree = false;
                 i++;
             } while (isTargetFieldFree && i < snake.getSegments().size());
 
             // Check if calculated Position is inhibited by the wall
-            int j=0;
+            int j = 0;
             do {
-                if(wall.getSegments().get(j).getX() == foodXCoord &&
+                if (wall.getSegments().get(j).getX() == foodXCoord &&
                         wall.getSegments().get(j).getY() == foodYCoord) isTargetFieldFree = false;
                 j++;
             } while (isTargetFieldFree && j < wall.getSegments().size());
 
 
-        }while(!isTargetFieldFree);
-        position = new Position(foodXCoord,foodYCoord);
+        } while (!isTargetFieldFree);
+        position = new Position(foodXCoord, foodYCoord);
 
         int foodTypeNumber = (int) (Math.random() * FOOD_TYPES.length);
         foodType = FOOD_TYPES[foodTypeNumber];
