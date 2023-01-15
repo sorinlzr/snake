fd7b56cee178a4f057627e1ebf2c93b6efc1a5cb
LeitgebMathias <mathias.leitgeb@stud.fh-campuswien.ac.at>
2023-01-15 18:07:04 +0100

    SNEK 16 - Rebase with Main, and some minor change.

--- src/main/java/at/ac/fhcampuswien/snake/controller/GameOverController.java
+++ src/main/java/at/ac/fhcampuswien/snake/controller/GameOverController.java
@@ -5,14 +5,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import static at.ac.fhcampuswien.snake.util.Constants.SCREEN_SIZE_MEDIUM;
 public class GameOverController {
     private Stage stage;
     private Scene scene;
